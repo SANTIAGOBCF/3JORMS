a6e74533dad2f5328c77d4c9d2068868c2393ae5
LeuGimrt <miguel.rodriguez18@unmsm.edu.pe>
2020-12-26 16:35:44 -0500

    footer v1

--- Desarrollo/DC19/Fuentes/DC19-Flask/run-server.bat
+++ Desarrollo/DC19/Fuentes/DC19-Flask/run-server.bat
@@ -6,4 +6,4 @@ set MAIL_USERNAME=app.supp.20@gmail.com
 set MAIL_PASSWORD=appsup20
 pip install -r requeriments/r.txt && start flask run
 timeout 3 > NUL
-start chrome http://127.0.0.1:5000/
+start firefox http://127.0.0.1:5000/
