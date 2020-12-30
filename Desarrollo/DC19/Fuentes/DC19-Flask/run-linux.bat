ba510bf79458d97e73733a63ed6ab589539d9e58
warevil <60890246+warevil@users.noreply.github.com>
2020-12-30 11:16:59 -0500

    updated batch linux

---
+++ Desarrollo/DC19/Fuentes/DC19-Flask/run-linux.bat
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export FLASK_APP=dc19.py
+export DC19_ADMIN=admin@dc19.org
+export FLASK_DEBUG=1
+export MAIL_USERNAME=app.supp.20@gmail.com
+export MAIL_PASSWORD=appsup20
+pip3 install -r requirements/r.txt && flask run
+
+firefox http://127.0.0.1:5000/
