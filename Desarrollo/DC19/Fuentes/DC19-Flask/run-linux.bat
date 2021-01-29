78a2a411e100299577c55b759af11dfe12b01cf8
Jonathan Galarza <jgga19@gmail.com>
2021-01-28 20:45:52 -0500

    update para evitar conflictos

--- Desarrollo/DC19/Fuentes/DC19-Flask/run-linux.bat
+++ Desarrollo/DC19/Fuentes/DC19-Flask/run-linux.bat
@@ -5,6 +5,6 @@ export DC19_ADMIN=admin@dc19.org
 export FLASK_DEBUG=1
 export MAIL_USERNAME=app.supp.20@gmail.com
 export MAIL_PASSWORD=appsup20
-pip3 install -r requirements/r.txt && flask run
+pip3 install -r requeriments/r.txt && flask run
 
 firefox http://127.0.0.1:5000/
