4d9aa102a9447ef909068c9f031811db3e4ce52a
Omar <omar.villarreal@unmsm.edu.pe>
2021-02-03 11:19:34 -0500

    linea base 04

---
+++ Línea Base/DC19/Linea Base 04/DC19-Flask/dc19.py
@@ -0,0 +1,34 @@
+import os
+
+import click
+# import waitress
+from flask_migrate import Migrate
+
+from app import create_app, db
+from app.models import User, Role, Permission, Post
+
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+migrate = Migrate(app, db)
+if __name__ == "__main__":
+    app.debug = False
+    port = int(os.environ.get('PORT', 33507))
+    waitress.serve(app, port=port)
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User, Role=Role, Permission=Permission, Post=Post)
+
+
+@app.cli.command()
+@click.argument('test_names', nargs=-1)
+def test(test_names):
+    """Run the unit tests."""
+    import unittest
+    if test_names:
+        tests = unittest.TestLoader().loadTestsFromNames(test_names)
+    else:
+        tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+
+
---
+++ Línea Base/DC19/Linea Base 04/DC19-Flask/debug.bat
@@ -0,0 +1,7 @@
+@echo off
+set FLASK_APP=dc19.py
+set DC19_ADMIN=admin@dc19.org
+set FLASK_DEBUG=1
+set MAIL_USERNAME=app.supp.20@gmail.com
+set MAIL_PASSWORD=jylxdygtzzeqjsyz
+flask shell
